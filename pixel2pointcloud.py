7eb863c0ff992dd7fd7a6a27b5789adfa73c5cf2
“YukiKuma111” <1343879876@qq.com>
2024-10-09 02:47:22 +0800

    [feature/pixel2pointcloud] Fulfill the function of creating dot lines on the corresponding point cloud.

--- pixel2pointcloud.py
+++ pixel2pointcloud.py
@@ -1,8 +1,14 @@
-import json
 import argparse
-import os
-import numpy as np
 import cv2
+import json
+import numpy as np
+import open3d as o3d
+import os
+import rosbag
+import tf
+from scipy.spatial.transform import Rotation as R
+
+from geometry_msgs.msg import TransformStamped
 
 
 class Pixel2PointCloud:
@@ -11,10 +17,17 @@ class Pixel2PointCloud:
             description="Convert pixel to point cloud data."
         )
         parser.add_argument(
-            "-pcd",
-            "--pointcloud",
+            "-ppcd",
+            "--panoramapointcloud",
             type=str,
-            default="pcd/",
+            default="pcd/0.pcd",
+            help="Path to the point cloud file",
+        )
+        parser.add_argument(
+            "-bag",
+            "--rosbag",
+            type=str,
+            default="rosbag/0.bag",
             help="Path to the point cloud file",
         )
         parser.add_argument(
@@ -40,16 +53,17 @@ class Pixel2PointCloud:
         )
 
         args = parser.parse_args()
-        self.pcd_folderpath = args.pointcloud
+        self.panorama_pcd = args.panoramapointcloud
+        self.bag_file = args.rosbag
         self.json_path = args.json
         self.save_path = args.save
         # 读取intrinsic.txt 和 extrinsic.txt
-        self.intrinsic_file = os.path.join(args.parameter, 'intrinsic.txt')
-        self.extrinsic_file = os.path.join(args.parameter, 'extrinsic.txt')
+        self.intrinsic_file = os.path.join(args.parameter, "intrinsic.txt")
+        self.extrinsic_file = os.path.join(args.parameter, "extrinsic.txt")
 
         # 检查点云文件夹是否存在
-        if not os.path.exists(self.pcd_folderpath):
-            print(f"Error: The folder {self.pcd_folderpath} does not exist.")
+        if not os.path.exists(self.panorama_pcd):
+            print(f"Error: The folder {self.panorama_pcd} does not exist.")
             return
         if not os.path.exists(self.intrinsic_file):
             print(f"Error: The folder {self.intrinsic_file} does not exist.")
@@ -57,44 +71,137 @@ class Pixel2PointCloud:
         if not os.path.exists(self.extrinsic_file):
             print(f"Error: The folder {self.extrinsic_file} does not exist.")
             return
-        
+
         # 定义并初始化变量
-        self.pixel_data = {}
         self.intrinsic_matrix = []
-        self.distortion_coeffs = [0, 0, 0, 0, 0]    # 因为解包时已去畸变
+        self.distortion_coeffs = []
         self.R_mat = []
         self.tvec = []
+        self.target_frame = "body"
+        self.source_frame = "camera_init"
 
         self.get_parameters()
+        rvec, _ = cv2.Rodrigues(self.R_mat)
 
         # 获取并排序文件夹中的 PCD 文件
-        self.pcd_filelist = self.get_sorted_pcd()
-        
-        self.pixel_data = self.load_json_data()
-        
-        for img_tsp, lines in self.pixel_data.items():
-            pcd_filepath = self.closest_pcd_tsp_filepath(img_tsp)
-            print(f"Image Timestamp:\t{img_tsp}.png")
-            print(f"Point cloud Timestamp:\t{os.path.basename(pcd_filepath)}")
-            # 打印对应的线段数据
-            for i, line in enumerate(lines, 1):
-                # for j, pxl in enumerate(line):
-                for pxl in line:
-                    lidar_xyz = self.pixel_to_3d_point(pxl[0], pxl[1])
-                    print(f"  pixel {i}: {pxl}")
-                    print(f"  pcd   {i}: {lidar_xyz.T}")
-                    # print(f"???Line {i}:{j} {line[j][0]}")
-                    # print(f"???Line {i}:{j} {line[j][1]}")
-                # print(f"  Line {i}: {line}")
+        # self.pcd_filelist = self.get_sorted_pcd()
 
+        pixel_data = self.load_json_data()
+        tf_data = self.parse_tf_from_bag()
+        panorama_pcd = o3d.io.read_point_cloud(self.panorama_pcd)
+        projection_pcd = o3d.io.read_point_cloud(self.panorama_pcd)
+        lines_pcd = o3d.geometry.PointCloud()
+
+        # for img_tsp, img_data in pixel_data.items():
+        for img_tsp, data in pixel_data.items():
+            tf_tsp_list = list(tf_data.keys())
+            tf_tsp = self.closest_tsp(img_tsp, tf_tsp_list)
+            print(f"IMG Timestamp:\t{img_tsp}.png")
+            print(f"TF Timestamp:\t{tf_tsp}")
+
+            # 找到对应时间戳的 translation 和 rotation_matrix
+            translation = tf_data[tf_tsp]["translation"]
+            rotation_matrix = np.linalg.inv(tf_data[tf_tsp]["rotation_matrix"])
+
+            # 计算欧拉角（需要yaw对后续找到的地面点做筛查？）
+            # r = R.from_matrix(rotation_matrix)
+            # euler_angles = r.as_euler("xyz", degrees=True)
+            # print("euler_angles: ", euler_angles)
+
+            # 裁剪全景点云，减少计算量
+            roi_points = self.filter_point_cloud(
+                panorama_pcd,
+                translation[0] - 10,
+                translation[0] + 10,
+                translation[1] - 10,
+                translation[1] + 10,
+                translation[2] - 10,
+                translation[2] + 10,
+            )
+
+            # 将 points 转换为 open3d.geometry.PointCloud 对象
+            roi_pcd = o3d.geometry.PointCloud()
+            roi_pcd.points = o3d.utility.Vector3dVector(np.array(roi_points))
+            # o3d.io.write_point_cloud(img_tsp + "cliped.pcd", roi_pcd)  # for debug
+
+            # 对点云进行旋转和平移操作
+            points = np.asarray(roi_pcd.points)
+            transformed_points = points - translation  # 平移
+            rotated_points = np.dot(transformed_points, rotation_matrix.T)  # 旋转
+            projection_pcd.points = o3d.utility.Vector3dVector(rotated_points)
+            # o3d.io.write_point_cloud(
+            #     img_tsp + "transformed_point_cloud.pcd", projection_pcd
+            # )  # for debug
+
+            # 筛选投影点云（一般来说要配合相机FOV，这里先直接hardcode）
+            filtered_pts = self.filter_point_cloud(
+                projection_pcd, -1, 10, -2, 2, -10, -0.1
+            )
+
+            # 投影3D点到2D图像视图
+            projected_pts, _ = cv2.projectPoints(
+                np.array(filtered_pts),
+                rvec,
+                self.tvec,
+                self.intrinsic_matrix,
+                self.distortion_coeffs,
+            )
+
+            # 构建字典，关联 2D 投影点和 3D 点云坐标
+            projection_dict = {}
+            for i, point_3d in enumerate(filtered_pts):
+                point_2d = tuple(projected_pts[i][0])  # 投影后的2D坐标
+                projection_dict[point_2d] = point_3d
+
+            start_end_points_list = []  # 只有和pixel一一对应的起点，中间的点，和终点
+            start2end_points_list = []  # 按照一定密度生成的点
+            # 打印对应的线段数据并保存最近的3D点
+            for i, line in enumerate(data["lines"], 1):
+                line_3d_points = []
+                for pxl in line:
+                    print(f"  pixel {i}: {pxl}")
+                    nearest_3d_point = self.find_nearest_2d_point(
+                        [pxl[0], pxl[1]], projection_dict
+                    )
+                    print(
+                        f"  Nearest 3D point: [{nearest_3d_point[0]:.2f}, {nearest_3d_point[1]:.2f}, {nearest_3d_point[2]:.2f}]"
+                    )
+
+                    # 将 nearest_3d_point 加入 pixel_data
+                    line_3d_points.append(nearest_3d_point)
+
+                start_end_points_list.append(line_3d_points)
+                # 将每个线段的对应 3D 点保存到 "points" 列表中
+                pixel_data[img_tsp]["points"] = start_end_points_list
+
+                # 生成等间隔的三维点
+                start2end_points_list = self.generate_dot_lines(line_3d_points)
+
+                # 扁平化 start_end_points_list，将所有 3D 点提取出来
+                flat_line_3d_points = [
+                    point for line in start2end_points_list for point in line
+                ]
+
+                # 将这些新的 3D 点追加到 filtered_pts 中
+                filtered_pts.extend(flat_line_3d_points)
+
+            filtered_pcd = o3d.geometry.PointCloud()
+            filtered_pcd.points = o3d.utility.Vector3dVector(np.array(filtered_pts))
+            o3d.io.write_point_cloud(
+                img_tsp + "filtered_pcdwpoints.pcd", filtered_pcd
+            )  # for debug
+
+        # 追加数据到JSON文件（不会覆盖之前的数据）
+        with open("new.json", "w") as json_file:
+            json.dump(pixel_data, json_file, indent=4)
 
     def load_json_data(self):
         """
-        读取JSON文件并返回一个字典，字典包含每个时间戳和对应的线段数据。
-        
+        读取JSON文件并返回一个字典,字典包含每个时间戳和对应的线段数据。
+
         参数:
         file_path: JSON文件的路径
-        
+
         返回:
         一个包含时间戳为键，线段列表为值的字典
         """
@@ -102,15 +209,19 @@ class Pixel2PointCloud:
         result = {}
 
         # 读取 JSON 文件
-        with open(self.json_path, 'r') as file:
+        with open(self.json_path, "r") as file:
             data = json.load(file)
 
         # 将数据组织成以时间戳为键，线段为值的字典
         for entry in data:
             timestamp = entry["timestamp"]
             lines = entry["lines"]
-            result[timestamp] = lines
-        
+            # 初始化每个时间戳对应的字典，并将lines加入
+            result[timestamp] = {
+                "lines": lines,
+                "points": [],  # 初始化一个空的points列表
+            }
+
         return result
 
     def get_sorted_pcd(self):
@@ -129,7 +240,7 @@ class Pixel2PointCloud:
             )
         )
         return pcd
-    
+
     def get_timestamp(self, filepath):
         # 获取文件名作为时间戳，去掉文件扩展名
         timestamp = (
@@ -138,55 +249,151 @@ class Pixel2PointCloud:
             + os.path.basename(filepath).split(".")[1]
         )
         return timestamp
-    
-    def closest_pcd_tsp_filepath(self, img_tsp):
+
+    def closest_tsp(self, target_tsp, tsp_list):
         crr_error = 0
         pre_error = None
-        for i in range(len(self.pcd_filelist)):
-            pcd_tsp = self.get_timestamp(self.pcd_filelist[i])
-            crr_error = abs(float(img_tsp) - float(pcd_tsp))
+        for i in range(len(tsp_list)):
+            tsp = tsp_list[i]
+            crr_error = abs(float(target_tsp) - float(tsp))
             if pre_error == None or pre_error >= crr_error:
                 pre_error = crr_error
                 continue
             else:
-                trg_tsp = self.pcd_filelist[i-1]
+                closest_tsp = tsp_list[i - 1]
                 break
-        return trg_tsp
-    
+        return closest_tsp
+
     def get_parameters(self):
         # 读取intrinsic矩阵和distortion系数
         def parse_line(line):
             """Helper function to parse a line and convert to floats with 5 decimal places."""
-            return [round(float(val.strip().replace(';', '')), 5) for val in line.split()]
+            return [
+                round(float(val.strip().replace(";", "")), 5) for val in line.split()
+            ]
 
-        with open(self.intrinsic_file, 'r') as f:
+        with open(self.intrinsic_file, "r") as f:
             lines = f.readlines()
             self.intrinsic_matrix = np.array([parse_line(line) for line in lines[1:4]])
-            # distortion_coeffs = np.array(parse_line(lines[6]))
+            self.distortion_coeffs = np.array(parse_line(lines[6]))
 
         # 读取extrinsic矩阵并提取旋转矩阵和平移矩阵
-        with open(self.extrinsic_file, 'r') as f:
+        with open(self.extrinsic_file, "r") as f:
             lines = f.readlines()
             extrinsic_matrix = np.array([parse_line(line) for line in lines[1:4]])
 
         self.R_mat = extrinsic_matrix[:, :3]
         self.tvec = extrinsic_matrix[:, 3]
-    
-    def pixel_to_3d_point(self, u, v):
-        Z = 10  # 深度值?????????????????????????????????????????????????????
 
-        # 将像素坐标转换为齐次坐标
-        uv_homogeneous = np.array([u, v, 1.0], dtype=np.float32).reshape(3, 1)
+    def parse_tf_from_bag(self):
+        # 打开rosbag文件
+        bag = rosbag.Bag(self.bag_file)
 
-        # 反投影到相机坐标系
-        intrinsic_inv = np.linalg.inv(self.intrinsic_matrix)
-        extrinsic_inv = np.linalg.inv(self.R_mat)
-        xyz_camera = intrinsic_inv @ uv_homogeneous * Z
+        listener = tf.TransformerROS()
+        tf_dict = {}
 
-        # 转换到世界坐标系
-        xyz_lidar_coor = extrinsic_inv @ xyz_camera 
-        
-        return xyz_lidar_coor
+        # 遍历bag文件的/tf话题
+        for topic, msg, t in bag.read_messages(topics=["/tf"]):
+            # msg.transforms 是一个包含多个 tf 变换的列表
+            for transform in msg.transforms:
+                # 检查是否是我们感兴趣的目标帧和源帧的变换
+                if (
+                    transform.child_frame_id == self.target_frame
+                    and transform.header.frame_id == self.source_frame
+                ):
+                    # 提取时间戳
+                    timestamp = transform.header.stamp.to_sec()
+
+                    # 提取平移和旋转四元数
+                    translation = np.array(
+                        [
+                            transform.transform.translation.x,
+                            transform.transform.translation.y,
+                            transform.transform.translation.z,
+                        ]
+                    )
+
+                    rotation_quaternion = np.array(
+                        [
+                            transform.transform.rotation.x,
+                            transform.transform.rotation.y,
+                            transform.transform.rotation.z,
+                            transform.transform.rotation.w,
+                        ]
+                    )
+
+                    # 使用 tf 将四元数转换为 3x3 旋转矩阵
+                    rotation_matrix = tf.transformations.quaternion_matrix(
+                        rotation_quaternion
+                    )[:3, :3]
+
+                    # 将 translation 和 rotation 存储到字典
+                    tf_dict[timestamp] = {
+                        "translation": translation,
+                        "rotation_matrix": rotation_matrix,
+                    }
+        bag.close()
+
+        return tf_dict
+
+    # 通过2D像素点查找对应的3D点云坐标
+    def find_nearest_2d_point(self, target_2d, projection_dict):
+        # 计算字典中所有2D点与目标点的距离
+        distances = {
+            pt_2d: np.linalg.norm(np.array(pt_2d) - np.array(target_2d))
+            for pt_2d in projection_dict.keys()
+        }
+
+        # 找到最近的2D点
+        nearest_2d = min(distances, key=distances.get)
+
+        # 返回最近2D点对应的3D点云坐标
+        return projection_dict[nearest_2d]
+
+    def filter_point_cloud(self, input_pcd, xmin, xmax, ymin, ymax, zmin, zmax):
+        filtered_pts = []
+        for point in np.asarray(input_pcd.points):
+            if (
+                xmin < point[0] < xmax
+                and ymin < point[1] < ymax
+                and zmin < point[2] < zmax
+            ):
+                filtered_pts.append((point[0], point[1], point[2]))
+        return filtered_pts
+
+    def generate_dot_lines(self, start_end_points, interval=0.01):
+        """
+        在至少两点 start_end_points 之间生成等间隔的三维点。
+
+        参数:
+        start_end_points: 两个以上的三维点的坐标 (x, y, z) 的list
+        interval: 每个新点的间隔距离，默认为 0.01
+
+        返回:
+        包含新生成点的列表
+        """
+        dot_lines = []
+
+        for i in range(1, len(start_end_points)):
+            print(f"from\t{start_end_points[i-1]}\nto\t{start_end_points[i]}")
+            # 将点转换为 numpy 数组
+            p1 = np.array(start_end_points[i - 1])
+            p2 = np.array(start_end_points[i])
+
+            # 计算两点之间的距离
+            distance = np.linalg.norm(p2 - p1)
+
+            # 计算需要生成多少个点
+            num_points = int(distance // interval)
+
+            # 计算方向向量
+            direction = (p2 - p1) / distance
+
+            # 生成等间隔的新点
+            points = [p1 + i * interval * direction for i in range(num_points + 1)]
+            dot_lines.append(points)
+
+        return dot_lines
 
 
 if __name__ == "__main__":
